056350fed9adc2dd40963e8f5944ab4785ab0c92
Jhasija9 <jhasija95@gmail.com>
2025-03-24 20:14:28 -0400

    visit data Ui changes

--- src/app/visit-data/visit-data.component.ts
+++ src/app/visit-data/visit-data.component.ts
@@ -5,19 +5,33 @@ import { Router, RouterModule } from '@angular/router';  // Add this import
 import { VisitData } from '../models/visit-data';
 import { VisitDataService } from '../services/visit-data.service';
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-visit-data-table',
   standalone: true,
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule  // Just import HttpClientModule directly
+    RouterModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatIconModule  // Just import HttpClientModule directly
   ],
   templateUrl: './visit-data.component.html',
   styleUrls: ['./visit-data.component.css']
 })
 export class VisitDataComponent implements OnInit {
   visitData: VisitData[] = [];
+  filteredVisitData: VisitData[] = [];
+  selectedDate: Date | null = new Date();
 
   constructor(
     private readonly http: HttpClient,
@@ -33,12 +47,35 @@ export class VisitDataComponent implements OnInit {
       .subscribe({
         next: (data) => {
           this.visitData = data;
+          this.filterVisitsByDate(this.selectedDate)
           console.log('data :>> ', data);
         },
         error: (error) => {
           console.error('Error fetching data:', error);
         }
       });
+  }
+  filterVisitsByDate(date: Date | null): void {
+    if (!date) {
+      this.filteredVisitData = this.visitData;
+      return;
+    }
+
+    const dateStr = date.toISOString().split('T')[0];
+    this.filteredVisitData = this.visitData.filter(visit => {
+      const visitDate = new Date(visit.start).toISOString().split('T')[0];
+      return visitDate === dateStr;
+    });
+  }
+
+  onDateSelected(event: any): void {
+    this.selectedDate = event.value;
+    this.filterVisitsByDate(this.selectedDate);
+  }
+  
+  clearDateFilter(): void {
+    this.selectedDate = null;
+    this.filteredVisitData = this.visitData;
   }
 
   onWDClick(row: VisitData): void {
